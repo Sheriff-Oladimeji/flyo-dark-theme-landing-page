5b1597fa571383da8638532d241a5949abf2549d
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-03-18 19:16:29 +0100

    remove bugs

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,11 @@ import {
   Footer,
   About,
 } from "./components";
-import styles from "./style";
-import "./index.css"
 
 const App = () => {
   return (
     <div className="bg-DarkBlueIntro text-[14px] py-6">
-      <div className={styles.container}>
-        <Navbar />
-        <Hero />
-      </div>
-
-      <section className={`w-full ${styles.container}`}>
-        <About />
-      </section>
+      <Navbar />
     </div>
   );
 };
