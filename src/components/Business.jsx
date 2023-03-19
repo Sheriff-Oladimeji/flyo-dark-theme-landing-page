189e85edf5f06219fc12be677a9d68fef1cf94b7
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-03-19 14:31:34 +0100

    update styles

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,13 +3,13 @@ import { productive , arrow} from "../assets"
 const Business = () => {
   return (
     <section className="text-white bg-DarkBlueMain w-full">
-<div className="flex flex-col md:flex-row items-center justify-center">
-<img src={productive} alt="stay productive"  className="w-[80%]"/>
+<div className="flex flex-col md:flex-row items-center justify-center w-[90%] mx-auto  gap-14">
+<img src={productive} alt="stay productive"  className="w-[90%] md:w-[50%]"/>
 <div>
-<h2>Stay productive, wherever you are</h2>
-<p>Never let location be an issue when accessing your files.Flyo has you covered for all of your file storage needs.</p>
-<p>Securely share files and folders wit friends,family and colleagues for live collaboration.No email attachmnts required.</p>
-<a href="#">See how Flyo works</a>
+<h2 className="text-White text-[2rem] font-bold font-heading z-20">Stay productive, <br className="hidden md:block"/> wherever you are</h2>
+< p className="text-White text-[16px] font-normal font-body  max-w-[450px] mt-4 z-20">Never let location be an issue when accessing your files.Flyo has you covered for all of your file storage needs.</p>
+< p className="text-White text-[16px] font-normal font-body t max-w-[450px] mt-4 z-20">Securely share files and folders wit friends,family and colleagues for live collaboration.No email attachmnts required.</p>
+<a href="#">See how Flyo works <img src={arrow} alt="arrow" /></a>
 </div>
 </div>
     </section>
