45a1987a2933946688242e85d00013429abd0967
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-03-20 12:23:31 +0100

    add hver state for nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 <img src={logo} alt="logo" className="w-[120px]" />
 <div className="flex space-x-8  md:space-x-12">
   {navElement.map((nav)=>(
-   <a className="text-White hover:font-bold cursor-pointer">{nav.text}</a>
+   <a className="text-White hover:font-bold cursor-pointer hover:underline">{nav.text}</a>
   ))}
 </div>
    </nav> 
