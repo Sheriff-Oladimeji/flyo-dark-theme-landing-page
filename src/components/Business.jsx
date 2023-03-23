7b287c8e7d640b5c0371779b8a44fbb383199e60
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-03-23 20:53:50 +0100

    fix bugs

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -9,7 +9,7 @@ const Business = () => {
 <h2 className="text-White text-[2rem] font-bold font-heading z-20">Stay productive, <br className="hidden md:block"/> wherever you are</h2>
 < p className="text-White text-[16px] font-normal font-body  max-w-[500px] mt-4 z-20">Never let location be an issue when accessing your files.Flyo has you covered for all of your file storage needs.</p>
 < p className="text-White text-[16px] font-normal font-body t max-w-[510px] mt-4 z-20">Securely share files and folders wit friends,family and colleagues for live collaboration.No email attachmnts required.</p>
-<a href="#" className="text-[16px] text-Cyan flex gap-2 border-b border-Cyan pb-1 w-[170px] font-heading mt-3">See how Flyo works <img src={arrow} alt="arrow" className="w-[10%]"/></a>
+<a href="#" className="text-[16px] text-Cyan flex gap-2 border-b border-Cyan pb-1 max-w-[180px] font-heading mt-3 items-center object-contain">See how Flyo works <img src={arrow} alt="arrow" /></a>
 
 </div>
 </div>
